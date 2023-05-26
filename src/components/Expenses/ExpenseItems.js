e42cf45845ad34e6992747891bc9711d519622f2
bin2159 <bipinthms007@gmail.com>
2023-05-26 10:38:10 +0530

    using State hooks

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,17 +1,21 @@
 import "./ExpenseItems.css";
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Cards from "../UI/Cards";
 const ExpenseItems = (props) => {
+  const [amount,setAmount]=useState(props.amount)
+
   const clickHandler=()=>{
-    console.log('clicked')
+    setAmount(amount+100)
+
   }
   return (
     <Cards className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         title={props.title}
-        amount={props.amount}
+        amount={amount}
         location={props.location}
       />
       <button onClick={clickHandler}>Delete</button>
